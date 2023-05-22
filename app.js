0d0a63be70d5690a799b1c17290e7fda8fb3b2b8
followjahidul@gmail.com <followjahidul@gmail.com>
2023-05-22 15:34:09 +0600

    install eslint and prettier and a bunch of packag

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const exp = require('constants');
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // Middleware
+// console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
