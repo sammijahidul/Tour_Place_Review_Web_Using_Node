c18eb93eb8d46112395f21209f2b36beb9a9d9a9
followjahidul@gmail.com <followjahidul@gmail.com>
2023-05-22 12:44:25 +0600

    install dotenv & setting up environment variable

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
 const app = require('./app');
 
-const port = 4008
+const port = process.env.PORT || 4008;
 app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`);
 });
