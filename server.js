d3b070c38891684a9edd9d29c2faaaf2270946a5
followjahidul@gmail.com <followjahidul@gmail.com>
2023-05-23 19:36:31 +0600

    connected the system with mongodb using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({path: './config.env'});
-const app = require('./app');
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB)
+    .then(() => {
+    console.log("Db is connected");
+    })
+    .catch(error => console.log(error));
+ 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+})
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Forest Hicker',
+    rating: 4.6,
+    price: 120
+})
+testTour
+    .save()
+    .then(doc => { console.log(doc) })
+    .catch(err => { console.log('Error', err)});
 
 const port = process.env.PORT || 4008;
 app.listen(port, () => {
