4fce3314c266790388e4fb2edc874d3523f7115a
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-05 20:04:35 +0600

    install slugify and implement document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true,'A tour duration must required']
@@ -56,10 +58,20 @@ const tourSchema = new mongoose.Schema({
     toObject: { virtuals: true}
 }
 );
-
+// Virtual properties
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
-})
+});
+// Document Middleware
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc);
+//     next();
+// });
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
