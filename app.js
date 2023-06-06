19fe67e052f96194052d231cbdc524cc4869e1dc
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-07 00:10:32 +0600

    created a middleware to handle any wrong routes

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter)
 
+app.all('*', (req, res, next) => {
+    res.status(400).json({
+        status: 'fail',
+        message: `coudn't find this ${req.originalUrl} on this server`
+    })
+})
+
 module.exports = app;
