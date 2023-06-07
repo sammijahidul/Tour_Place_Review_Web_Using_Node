661a28e411f0e493cfbc327659dec4e081830892
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-07 15:03:10 +0600

    created global handler controller and refactoring

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -21,19 +23,12 @@ app.all('*', (req, res, next) => {
     //     status: 'fail',
     //     message: `coudn't find this ${req.originalUrl} on this server`
     // })
-    const err = new Error(`coudn't find this ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode= 404;
-    next(err);
+    // const err = new Error(`coudn't find this ${req.originalUrl} on this server`);
+    // err.status = 'fail';
+    // err.statusCode= 404;
+    next(new AppError(`coudn't find this ${req.originalUrl} on this server`));
 });
-
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-});
+// Asdfg12345
+app.use(globalErrorHandler);
 
 module.exports = app;
