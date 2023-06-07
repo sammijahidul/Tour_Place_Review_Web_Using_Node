3573f0006b7b394e107911d87132abf00d6f7a68
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-07 10:53:21 +0600

    implements global error handler

--- app.js
+++ app.js
@@ -17,10 +17,23 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
-    res.status(400).json({
-        status: 'fail',
-        message: `coudn't find this ${req.originalUrl} on this server`
+    // res.status(400).json({
+    //     status: 'fail',
+    //     message: `coudn't find this ${req.originalUrl} on this server`
+    // })
+    const err = new Error(`coudn't find this ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode= 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     })
-})
+});
 
 module.exports = app;
