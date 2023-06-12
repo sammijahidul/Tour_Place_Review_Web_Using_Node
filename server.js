e401d7f4f4737cb51992d02ea26f1b89b376b9c6
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-12 11:27:07 +0600

    unhandledrejection, uncaughtEception error solve

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
-
-
 const DB = process.env.DATABASE;
-
 mongoose.connect(DB)
     .then(() => {
     console.log("Db is connected");
     })
-    .catch(error => 
-        console.log(error)
-        );
+    // .catch(error => 
+    //     console.log('error')
+    //     );
  
 const port = process.env.PORT || 4008;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
 });
