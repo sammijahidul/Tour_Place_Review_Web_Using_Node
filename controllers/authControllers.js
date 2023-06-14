6dfc4a3b57f931766e6db850230ff0d3f1406fc7
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-14 11:36:34 +0600

    getalltours route get protected middleware

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,13 +7,13 @@ const AppError = require("../utils/appError");
 const signToken = id => jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
   })
-
 exports.signup = catchAsync (async (req, res, next) => {
     const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
+        passwordConfirm: req.body.passwordConfirm,
+        passwordChangedAt: req.body.passwordChangedAt
     });
     const token = signToken(newUser._id);
     res.status(201).json({
@@ -55,8 +55,16 @@ exports.protect = catchAsync (async (req, res, next) => {
     }
     // Verification token
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-    console.log(decoded);
-
-
+    // Check if youser still exist
+    const currentUser = await User.findById(decoded.id);
+    if(!currentUser) {
+        return next(new AppError('The user belongs to this token no longer exist', 401));
+    }
+    // Check the user changed his password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next (new AppError('User recently changed password, please log in again', 401))
+    };
+    // grant access to the protected route
+    req.user = currentUser;
     next();
 });
