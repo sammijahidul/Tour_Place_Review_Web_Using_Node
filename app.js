e547f595611d940dc12ea1d119097eb81a3e9a7c
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-16 01:19:49 +0600

    limit request, data sanitization, prevent hpp

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoseSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,21 +14,46 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middleware
-// console.log(process.env.NODE_ENV);
+//Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
-}
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+};
+// Limit request from same api
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against nosql query injection
+app.use(mongoseSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: ['duration', 'ratingsQuantity', 
+                'ratingsAverage', 'maxGroupSize', 
+                'difficulty', 'price']
+}));
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     console.log(req.headers);
     next();
 });
-
 app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
     // res.status(400).json({
@@ -34,7 +65,7 @@ app.all('*', (req, res, next) => {
     // err.statusCode= 404;
     next(new AppError(`coudn't find this ${req.originalUrl} on this server`));
 });
-// Asdfg12345
+
 app.use(globalErrorHandler);
 
 module.exports = app;
