76400fd64062bbbf2a11672b8b981c0e20cd9884
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-25 20:30:18 +0600

    geospatial aggregation calculation for distance

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -27,6 +27,9 @@ router
     .route('/tours-within/:distance/center/:latlng/unit/:unit')
     .get(tourController.getToursWithin);      
 router
+    .route('/distances/:latlng/unit/:unit')
+    .get(tourController.getDistances);
+router
     .route('/')
     .get(tourController.getAllTourData)
     .post(authController.protect, 
