8aa1e4fe596786b73282e0f9cac0d48453c1325c
followjahidul@gmail.com <followjahidul@gmail.com>
2023-06-26 15:27:11 +0600

    install and setup template engine pug

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +13,12 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
 //Set security HTTP headers
@@ -45,14 +51,17 @@ app.use(hpp({
                 'ratingsAverage', 'maxGroupSize', 
                 'difficulty', 'price']
 }));
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
+
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     console.log(req.headers);
     next();
 });
+app.get('/', (req, res) => {
+    res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
