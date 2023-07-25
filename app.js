cf1b12c403abca379adcaa08192b4890e4b6fbf4
followjahidul@gmail.com <followjahidul@gmail.com>
2023-07-25 23:35:49 +0600

    refactoring the codes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('././routes/viewRoutes');
 
 const app = express();
 
@@ -51,30 +52,14 @@ app.use(hpp({
                 'ratingsAverage', 'maxGroupSize', 
                 'difficulty', 'price']
 }));
-
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     console.log(req.headers);
     next();
 });
-app.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Forest Hiker',
-        user: 'Jonas'
-    });
-});
-app.get('/overview', (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
-});
-app.get('/tour', (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
-    });
-});
 
+// Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
