bfa9b8bc4c48e501baff497563abbc9d6333986f
followjahidul@gmail.com <followjahidul@gmail.com>
2023-08-10 23:58:29 +0600

    created booking model & booking session

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
-
 const viewsController = require('../controllers/viewsControllers');
 const authController = require('../controllers/authControllers');
+const bookingController = require('../controllers/bookingControllers');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/', 
+           bookingController.createBookingCheckout, 
+           authController.isLoggedIn, 
+           viewsController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
